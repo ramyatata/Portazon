a1c2896fe3c8241b02c208daf7511a9f88ebc475
Vitor Pereira <vpereira1982@MacBook-Pro.local>
2017-12-23 16:44:04 -0800

    updateSearch

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const server = express();
-const routes = require('./controller.js');
+const routes = require('./search/index.js');
 
 server.listen(process.env.PORT || 3000, () => {
   console.log('Server is awaiting request');
