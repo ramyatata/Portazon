89ffb5e2cc91be8cdad9655d31d133b94e3d6c70
lichin shao <lichinshao@gmail.com>
2018-01-17 21:38:21 -0800

    test commit

--- client/src/index.js
+++ client/src/index.js
@@ -38,7 +38,7 @@ class Hello extends React.Component {
   }
 
   render() {
-    console.log('rendering page', this.state.view)
+    console.log('rendering page why isn\'t this working!!!', this.state.view)
     return (
       <div>
         <div>Hello rendering using react!!!</div>
