08f572461e0444df92c31fab94caa2eaa56abceb
lichin shao <lichinshao@gmail.com>
2018-01-18 09:23:02 -0800

    pseudocoded what functions necessary for shopping cart

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import ShoppingCart from './components/Shoppingcart.js';
+
+import ShoppingCart from './components/Shoppingcart.jsx';
 
 
 class Hello extends React.Component {
   constructor(props) {
     super(props);
+    //we need to make tabs on the homepage that will change the view in state
+    //right now, the default page should be the homepage
+    //the user should be able to click on a "shopping cart" tab somewhere and it should rerender
+    //to be the shopping cart component
+    //we also need to implement the homepage and what it displays.
     this.state = {
-      view: 'shoppingCart'
+      view: 'homepage',
+      cart: []
     }
   }
 
@@ -25,13 +32,33 @@ class Hello extends React.Component {
     })
   }
 
+  addItemToCart(item) {
+    //this function should have an input of an object that represents a single item
+    //we then need to do an ajax POST request to the server to addItemToCart
+    //the data we need to send with this request is the item object
+    //upon success:
+    //we should update the users shopping cart by setting the state
+    //upon error:
+    //console log the error and see whats happening!
+  }
+
+  removeItemFromCart(item) {
+    //this function should have an input of an item object
+    //we then need to do an ajax POST request to the server to removeItemFromCart
+    //the data we need to send with this post request is the item object
+    //upon success:
+    //we should update the users cart to reflect the removed item
+    //upon error:
+    //console log the error and see whats happening!!
+  }
+
   renderView() {
-    console.log('in render view', this.state.view)
+
     let view = this.state.view;
     if (view === 'homepage') {
       return <HomePage/>
     } else if (view === 'shoppingCart') {
-      return <ShoppingCart />
+      return <ShoppingCart cart={this.state.cart}/>
     } else {
       return null;
     }
@@ -39,15 +66,20 @@ class Hello extends React.Component {
 
   render() {
     console.log('rendering page why isn\'t this working!!! test', this.state.view)
+    alert('rendering page!!!!')
     return (
       <div>
         <div>Hello rendering using react!!!</div>
+
         <div>
           {this.renderView()}
         </div>
+
       </div>
       );
   }
 }
 
 ReactDOM.render(<Hello/>, document.getElementById('root'));
+
+
