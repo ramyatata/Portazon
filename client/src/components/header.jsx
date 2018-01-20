b4cd1b6f99c67974ca79dd62b1cb1336b314b946
ramyatata <ramyatata007@gmail.com>
2018-01-19 17:10:19 -0800

    added header account dropdown

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -31,46 +31,40 @@
               </li>
             </ul>
 
-
-
-            <div className="input-group mb-3">
-  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-  <div className="input-group-append">
-    <span className="input-group-text" id="basic-addon2">@example.com</span>
-  </div>
-</div>
-
-
-
-
-
             {/* search form */}
-            <form className="navbar-form navbar-left">
-              <div className="input-group">
-                <input type="text" className="form-control" placeholder="Search for something fun ...."/>
-
-                <div className="input-group-append">
-     <button type="submit" className="btn btn-default input-gr"><span className='glyphicon glyphicon-search'></span></button>
-  </div>
-
+            <ul className="nav navbar-nav" style={{width: '50%'}}>
+              <div className="input-group" style={{paddingTop: '10px'}}>
+                <input type = "text" className ="form-control" placeholder="Search for something fun ..." />
+                <div className="input-group-addon">
+                  <span className="glyphicon glyphicon-search"></span>
+                </div>
               </div>
+            </ul>
 
-            </form>
 
             {/* navbar right */}
             <ul className="nav navbar-nav navbar-right">
-        <li><a href="#"><span className="glyphicon-glyphicon-shopping-cart"></span></a></li>
-        <li className="dropdown">
-          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="glyphicon glyphicon-user"></span></a>
-          <ul className="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" className="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
+              <li><a href="#"><span className="glyphicon glyphicon-shopping-cart" style={{'fontSize': '1.5em'}}></span></a></li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span className="glyphicon glyphicon-user" style={{'fontSize': '1.5em'}}></span></a>
+                <div className="dropdown-menu">
+                  <div className="headerAccountDropdown">
+                    <h3 style={{color: '#fff'}}>Login</h3>
+                  </div>
+                  <form method="post" action="/account/login" className='headerAccountDropdown'>
+                    <div className="form-group ">
+                      <input type="email" className="form-control"placeholder="Email"/>
+                    </div>
+                    <div className="form-group">
+                      <input type="password" className="form-control" placeholder="Password"/>
+                    </div>
+                    <button type="submit" className="btn btn-block">SIGN IN</button>
+                  </form>
+                  <a>Create an account? </a>
+                  <a className="dropdown-item" href="#">Forgot password?</a>
+                </div>
+              </li>
+            </ul>
 
           </div>
         </nav>
