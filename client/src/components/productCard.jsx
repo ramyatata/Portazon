45e93d93a846686a73fc67678eba88763999c0be
lichin shao <lichinshao@gmail.com>
2018-01-21 14:45:21 -0800

    implemented add item to cart in product card components

--- client/src/components/productCard.jsx
+++ client/src/components/productCard.jsx
@@ -5,8 +5,12 @@ class ProductCard extends React.Component {
   constructor(props){
     super(props);
   }
+
+  handleClick(item) {
+    this.props.addItemToCart(item);
+  }
+
   render(){
-    // console.log('in product card item:', this.props.item )
     let item = this.props.item;
     return(
       <div className='card col-xs-3' style={{margin: '10px', width: '30rem', border: 'solid 1px grey', padding:'20px 20px'}}>
@@ -15,7 +19,7 @@ class ProductCard extends React.Component {
           <h4 className='card-title'>{item._source.product_name}</h4>
           <p className='card-text'>{item._source.description}}</p>
           <h5 className='card-price'>{item._source.discounted_price.toFixed(2)}</h5>
-          <a href='#' className='btn btn-primary'>add to cart</a>
+          <a href='#' className='btn btn-primary' onClick={() => this.handleClick(item)}>add to cart</a>
         </div>
       </div>
     );
