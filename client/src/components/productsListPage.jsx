9fbc1ccbd9738d3e97a02f9d152a02910b33e16e
lichin shao <lichinshao@gmail.com>
2018-01-21 14:06:18 -0800

    implemented productList and ProductCard

--- client/src/components/productsListPage.jsx
+++ client/src/components/productsListPage.jsx
@@ -11,25 +11,28 @@ class ProductsListPage extends React.Component {
     this.state = {
       products: this.props.products
     }
+    this.createProductCards = this.createProductCards.bind(this);
   }
+
+  createProductCards() {
+    console.log('in create product card')
+    let products = this.state.products;
+    console.log('products', products)
+    return products.map((item, ind) => (
+        <ProductCard item={item} key={ind}/>
+      )
+    )
+  }
+
+
   render(){
-    console.log('products in product page', this.state.products)
+    // console.log('products in product page', this.state.products)
     return(
         <div>
           <Header></Header>
-          <h3>Robotics</h3>
+          <h3>You searched for: {this.props.query}</h3>
           <div className="col-xs-12">
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
-          <ProductCard></ProductCard>
+            {this.createProductCards()}
           </div>
           <Footer/>
         </div>
