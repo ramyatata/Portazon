b8fa21c4fefe72a7b26c3d72ffa33daf7cf5da27
Vitor Pereira <vpereira1982@gmail.com>
2018-01-25 19:50:04 -0200

    dataCleanUp_deleteIndexEndPoint

--- server/payment/index.js
+++ server/payment/index.js
@@ -31,7 +31,7 @@ router.post('/', function(req, res) {
             console.log(charge);
             res.sendStatus(200);
         }
-        
+
     })
 
 
--- server/search/index.js
+++ server/search/index.js
@@ -9,7 +9,8 @@ const {
   addDocument,
   createUser,
   searchUser,
-  createIndexMapping
+  createIndexMapping,
+  deleteIndex
 } = queries;
 
 
@@ -34,7 +35,6 @@ router.get('/user', (req, res) => {
 
 
 router.get('/', (req, res) => {
-  // console.log('in search', req.query)
   searchQuery(req.query).then((response) => {
     let hits = response.hits.hits;
 
@@ -52,6 +52,18 @@ router.get('/category', (req, res) => {
   })
 });
 
+//*************
+// GET Routing: Delete Index
+//*************
+
+router.get('/delete', (req, res) => {
+  deleteIndex('products')
+  .then(() => {
+    res.status(2004).send('Product index has been deleted');
+  })
+  .catch((err) => console.error(err));
+});
+
 
 //*************
 // POST Routing: User and New Index Creation
@@ -60,14 +72,19 @@ router.get('/category', (req, res) => {
 // Create User
 router.post('/createuser', (req, res) => {
   createUser(req.body)
-  .then(() => console.log(`${req.body.firstname} ${req.body.firstname} has been added to the User database`))
+  .then(() => {
+    res.status(201)
+    .send(`${req.body.firstname} ${req.body.firstname} has been added to the User database`);
+  })
   .catch((err) => console.error(err));
 });
 
 
 router.post('/createindex', (req, res) => {
   createIndexMapping(req.body)
-  .then(() => console.log(`${req.body} has been created`))
+  .then(() => {
+    res.status(201).send(`${req.body} has been created`);
+  })
   .catch((err) => console.error(err));
 });
 
