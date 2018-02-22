30feec7bd80d3d95ac44b82a56d4212d0943d0e9
lichin shao <lichinshao@gmail.com>
2018-02-21 20:42:23 -0800

    added success component to render after a user has been registered

---
+++ client/src/components/registerSuccess.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const RegisterSuccess = () => {
+  return (
+    <div>
+      <h2>Success! Your account has been created.</h2>
+      <h3>Please Log In to continue.</h3>
+    </div>
+  )
+}
+
+export default RegisterSuccess;
