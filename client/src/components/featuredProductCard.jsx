251136b1a1996dcc038456c678b831ba65b7c3a4
ramyatata <ramyatata007@gmail.com>
2018-02-22 20:48:22 -0800

    carousel styling

--- client/src/components/featuredProductCard.jsx
+++ client/src/components/featuredProductCard.jsx
@@ -11,9 +11,9 @@ class FeaturedProductCard extends React.Component {
     //console.log(this.props.product);
     let image = JSON.parse(this.props.product._source.image)[0];
     return (
-      <div className="item" style={{'margin-left': '10px'}}>
-        <div className="col-xs-2">
-          <img className="carousel-img" src={image} style={{'width': '200px', 'height': '150px', overflow: 'hidden'}}/>
+      <div className="item" >
+        <div className="col-xs-2" style={{'padding': '20px'}}>
+          <img className="carousel-img" src={image} style={{'width': '150px', 'height': '150px', overflow: 'hidden'}}/>
           <div className="carousel-title">
             <h4>$ {this.props.product._source.discounted_price}</h4>
             <span style={{align:'text-center'}}>{this.props.product._source.product_name}</span>
