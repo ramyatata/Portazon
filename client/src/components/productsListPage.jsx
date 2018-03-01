a4ec15af7b7ae6e165d35963860a4d513bb519c9
ramyatata <ramyatata007@gmail.com>
2018-03-01 12:04:43 -0800

    minor changes

--- client/src/components/productsListPage.jsx
+++ client/src/components/productsListPage.jsx
@@ -5,9 +5,9 @@ import { withRouter } from 'react-router-dom';
 import ProductCard from './productCard.jsx';
 
 const ProductsListPage = ({products, query, addItemToCart, changeView}) => (
-  <div>
-    <h3>You searched for: {query}</h3>
-    <div className="col-xs-12">
+  <div className="col-xs-12">
+    <h5 className={{'paddingLeft': '5px'}}>Search: {query}</h5>
+    <div>
     {products.map((item, ind) => (
         <ProductCard item={item} key={ind} addItemToCart={addItemToCart} changeView={changeView}/>
       )
