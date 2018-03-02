bf1262e55b9cda363fde63efd9df0c934bda93df
ramyatata <ramyatata007@gmail.com>
2018-03-02 12:53:14 -0800

    Featured products button added

--- client/src/components/featuredProductCard.jsx
+++ client/src/components/featuredProductCard.jsx
@@ -4,19 +4,20 @@ import ReactDOM from 'react-dom';
 class FeaturedProductCard extends React.Component {
   constructor(props){
     super(props);
-
   }
 
   render() {
-    //console.log(this.props.product);
     let image = JSON.parse(this.props.product._source.image)[0];
     return (
       <div className="item" >
         <div className="col-xs-2" style={{'padding': '20px'}}>
           <img className="carousel-img" src={image} style={{'width': '150px', 'height': '150px', overflow: 'hidden'}}/>
           <div className="carousel-title">
-            <h4>$ {this.props.product._source.discounted_price}</h4>
-            <span style={{align:'text-center'}}>{this.props.product._source.product_name}</span>
+            <h5>$ {this.props.product._source.discounted_price}</h5>
+            <div style={{height: '60px', overflow: 'hidden'}}>
+              <span style={{align:'text-center'}}>{this.props.product._source.product_name}</span>
+            </div>
+            <button className="btn btn-cart">Shop This Product</button>
           </div>
         </div>
       </div>
