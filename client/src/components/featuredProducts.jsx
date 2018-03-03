c8f9e2105f1d1caae539eb8160929cdb6d69199d
ramyatata <ramyatata007@gmail.com>
2018-03-03 10:04:30 -0800

    carousel click

--- client/src/components/featuredProducts.jsx
+++ client/src/components/featuredProducts.jsx
@@ -36,7 +36,8 @@ class FeaturedProducts extends React.Component {
   }
 
   render() {
-    let highRatedProducts = []
+    let highRatedProducts = [];
+
     if(this.props.featuredProducts.length === 0){
       highRatedProducts = <div></div>
     } else {
@@ -48,7 +49,7 @@ class FeaturedProducts extends React.Component {
         <div className="col-xs-12 text-center"><h3>High Rated Products</h3></div>
         <div className="col-xs-12">
           <div className="carousel slide" id="myCarousel">
-            <div className="carousel-inner">
+            <div className="carousel-inner" >
               {highRatedProducts}
             </div>
             <a className="left carousel-control" href="#myCarousel" data-slide="prev"><i className="glyphicon glyphicon-chevron-left"></i></a>
@@ -61,27 +62,3 @@ class FeaturedProducts extends React.Component {
 }
 
 export default FeaturedProducts;
-
-
-
-// <div className="item">
-//       <div className="col-lg-2"><a href="#"><img src="http://placehold.it/500/e477e4/fff&amp;text=2" className="img-responsive"/></a></div>
-//     </div>
-//     <div className="item">
-//       <div className="col-lg-2"><a href="#"><img src="http://placehold.it/500/eeeeee&amp;text=3" className="img-responsive"/></a></div>
-//     </div>
-//     <div className="item">
-//       <div className="col-lg-2"><a href="#"><img src="http://placehold.it/500/f4f4f4&amp;text=4" className="img-responsive"/></a></div>
-//     </div>
-//     <div className="item">
-//       <div className="col-lg-2"><a href="#"><img src="http://placehold.it/500/f566f5/333&amp;text=5" className="img-responsive"/></a></div>
-//     </div>
-//     <div className="item">
-//       <div className="col-lg-2"><a href="#"><img src="http://placehold.it/500/f477f4/fff&amp;text=6" className="img-responsive"/></a></div>
-//     </div>
-//     <div className="item">
-//       <div className="col-lg-2"><a href="#"><img src="http://placehold.it/500/eeeeee&amp;text=7" className="img-responsive"/></a></div>
-//     </div>
-//     <div className="item">
-//       <div className="col-lg-2"><a href="#"><img src="http://placehold.it/500/fcfcfc/333&amp;text=8" className="img-responsive"/></a></div>
-//     </div>
